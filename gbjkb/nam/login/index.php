41a3bcb39b9cfb540b97aec261d963db2ff6499c
chucuoinb <laivannamyknb@gmail.com>
2016-04-02 16:01:09 +0700

    edit login (nam)

--- gbjkb/nam/login/index.php
+++ gbjkb/nam/login/index.php
@@ -5,4 +5,9 @@
  * Date: 4/2/2016
  * Time: 12:49 AM
  */
+require_once('../function/const.php');
+require_once('../function/function.php');
+require_once('../function/loader.php');
+$username = $_POST[USERNAME];
+$password = $_POST[PASSWORD];
 ?>
